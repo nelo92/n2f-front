74b7009be3d48c7e6cb87f9963361917167ed0fd
nelo92 <manuel.augusto.92@gmail.com>
2022-11-04 16:06:05 +0100

    login pwd start

---
+++ src/app/modules/auth/pages/forget-password-page/forget-password-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ForgetPasswordPageComponent } from './forget-password-page.component';
+
+describe('ForgetPasswordPageComponent', () => {
+  let component: ForgetPasswordPageComponent;
+  let fixture: ComponentFixture<ForgetPasswordPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ForgetPasswordPageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ForgetPasswordPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/auth/pages/forget-password-page/forget-password-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forget-password-page',
+  templateUrl: './forget-password-page.component.html',
+  styleUrls: ['./forget-password-page.component.css']
+})
+export class ForgetPasswordPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
