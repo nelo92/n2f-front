7c7929a47282f01e9fe18f5890db293b144deae3
nelo92 <manuel.augusto.92@gmail.com>
2022-11-04 15:57:15 +0100

    create release clean import in modules

---
+++ release-notes.md
@@ -0,0 +1,36 @@
+# N2F-FRONT RELEASE NOTES
+
+## v1.0.0
+
+* show version in application
+
+## v1.1.0
+
+* recalculate total after delete/deleteAll action in screen view
+
+## v1.1.0
+
+* Bug Fixes
+
+## v1.2.0
+
+* add authentification & data by user
+
+## v1.2.3
+
+* configure firebase rewrite url
+
+## v1.2.4
+
+* merge auth to master branch
+
+* upgrade dependencies of project
+
+## v1.3.0
+
+* change structure of project with notion module
+
+## v2.0.0
+
+* Improve selection month only in view page
+* Add chart to view year progressive expense
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
-import { AuthGuard } from './modules/auth/auth.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
-import { InputPageComponent } from './modules/notedefrais/pages/input-page/input-page.component';
-import { ViewPageComponent } from './modules/notedefrais/pages/view-page/view-page.component';
-import { SignInPageComponent } from './modules/auth/pages/sign-in-page/sign-in-page.component';
+import { AuthGuard } from './modules/auth/auth.guard';
 
 const routes: Routes = [
     { path: "", redirectTo: "/auth/sign-in", pathMatch: "full" },
